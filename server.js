0a8eb939ddd3adc2b6fcb4b14b65d3549ba6d568
SamiTahir09 <samitahir858@gmail.com>
2026-02-02 21:53:52 -0800

    setup

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("Server is running...");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+app.use(express.json());
+
+//db connection
+connectDb()
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on :${process.env.PORT}`);
+});
